d1d15d9d72cad517ddf97b34853c8747a117cf23
Manaure Vasconcelos <manaurevasc97@gmail.com>
2026-03-25 18:59:37 -0300

    refactor: Created a new folder for Endpoints and created TransactionsEndpoints

---
+++ Endpoints/TransactionsEndpoints.cs
@@ -0,0 +1,62 @@
+using Finance_API.Models;
+using FinanceAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceAPI.Endpoints
+{
+    public static class TransactionsEndpoints
+    {
+        public static void MapTransactionsEndpoints(this WebApplication app)
+        {
+            var transactionsGroup = app.MapGroup("/transactions");
+
+            transactionsGroup.MapGet("/", async (AppDbContext db) =>
+            {
+                return Results.Ok(await db.Transactions.ToListAsync());
+            });
+
+            transactionsGroup.MapGet("/{id}", async (AppDbContext db, int id) =>
+                await db.Transactions.FindAsync(id) is Transaction transactionFind
+                    ? Results.Ok(transactionFind)
+                    : Results.NotFound("Transaction not found.")
+            );
+
+            transactionsGroup.MapPost("/", async (AppDbContext db, Transaction transaction) =>
+            {
+                db.Transactions.Add(transaction);
+                await db.SaveChangesAsync();
+                return Results.Created($"/transactions/{transaction.Id}", transaction);
+            });
+
+            transactionsGroup.MapPut("/{id}", async (AppDbContext db, int id, Transaction newTransaction) =>
+            {
+                var transactionFind = await db.Transactions.FindAsync(id);
+                if (transactionFind is null)
+                    return Results.NotFound("Transaction not found.");
+
+                transactionFind.Amount = newTransaction.Amount;
+                transactionFind.Name = newTransaction.Name;
+                transactionFind.Description = newTransaction.Description;
+                transactionFind.Type = newTransaction.Type;
+                transactionFind.Category = newTransaction.Category; // modificar posteriormente => CategoryID
+                transactionFind.Date = newTransaction.Date;
+                transactionFind.UpdatedAt = DateTime.UtcNow;
+
+                await db.SaveChangesAsync();
+                return Results.NoContent();
+
+            });
+
+            transactionsGroup.MapDelete("/{id}", async (AppDbContext db, int id) =>
+            {
+                if (await db.Transactions.FindAsync(id) is Transaction transactionFind)
+                {
+                    db.Transactions.Remove(transactionFind);
+                    await db.SaveChangesAsync();
+                    return Results.NoContent();
+                }
+                return Results.NotFound("Transaction not found.");
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using FinanceAPI.Data;
-using Finance_API.Models;
+using FinanceAPI.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,56 +8,6 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data S
 
 var app = builder.Build();
 
-var transactionsGroup = app.MapGroup("/transactions");
-
-transactionsGroup.MapGet("/", async (AppDbContext db) =>
-{
-    return Results.Ok(await db.Transactions.ToListAsync());
-});
-
-transactionsGroup.MapGet("/{id}", async (AppDbContext db, int id) =>
-    await db.Transactions.FindAsync(id) is Transaction transactionFind
-        ? Results.Ok(transactionFind)
-        : Results.NotFound("Transaction not found.")
-);
-
-transactionsGroup.MapPost("/", async (AppDbContext db, Transaction transaction) =>
-{
-    db.Transactions.Add(transaction);
-    await db.SaveChangesAsync();
-    return Results.Created($"/transactions/{transaction.Id}", transaction);
-});
-
-transactionsGroup.MapPut("/{id}", async (AppDbContext db, int id, Transaction newTransaction) =>
-{
-    var transactionFind = await db.Transactions.FindAsync(id);
-    if (transactionFind is null)
-        return Results.NotFound("Transaction not found.");
-
-    transactionFind.Amount = newTransaction.Amount;
-    transactionFind.Name = newTransaction.Name;
-    transactionFind.Description = newTransaction.Description;
-    transactionFind.Type = newTransaction.Type;
-    transactionFind.Category = newTransaction.Category;
-    transactionFind.Date = newTransaction.Date;
-    transactionFind.UpdatedAt = DateTime.UtcNow;
-
-    await db.SaveChangesAsync();
-    return Results.NoContent();
-
-});
-
-transactionsGroup.MapDelete("/{id}", async (AppDbContext db, int id) =>
-{
-    if (await db.Transactions.FindAsync(id) is Transaction transactionFind)
-    {
-        db.Transactions.Remove(transactionFind);
-        await db.SaveChangesAsync();
-        return Results.NoContent();
-
-
-    }
-    return Results.NotFound("Transaction not found.");
-});
+app.MapTransactionsEndpoints();
 
 app.Run();
