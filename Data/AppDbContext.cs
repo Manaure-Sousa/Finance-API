8913229fef31d19b55e91928ed8c70b932c40267
Manaure Vasconcelos <manaurevasc97@gmail.com>
2026-03-26 17:29:06 -0300

    feat: Add SeedCategories

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Finance_API.Models;
+using FinanceAPI.Models.Enums;
 
 namespace FinanceAPI.Data
 {
@@ -7,5 +8,26 @@ namespace FinanceAPI.Data
     {
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Alimentação", Type = TransactionType.Expense },
+                new Category { Id = 2, Name = "Transporte", Type = TransactionType.Expense },
+                new Category { Id = 3, Name = "Salário", Type = TransactionType.Income },
+                new Category { Id = 4, Name = "Investimentos", Type = TransactionType.Investment },
+                new Category { Id = 5, Name = "Lazer", Type = TransactionType.Expense },
+                new Category { Id = 6, Name = "Moradia", Type = TransactionType.Expense },
+                new Category { Id = 7, Name = "Saúde", Type = TransactionType.Expense },
+                new Category { Id = 8, Name = "Educação", Type = TransactionType.Expense },
+                new Category { Id = 9, Name = "Assinaturas", Type = TransactionType.Expense },
+                new Category { Id = 10, Name = "Compras", Type = TransactionType.Expense },
+                new Category { Id = 11, Name = "Freelance", Type = TransactionType.Income },
+                new Category { Id = 13, Name = "Dividendos", Type = TransactionType.Investment },
+                new Category { Id = 14, Name = "Rendimentos", Type = TransactionType.Investment },
+                new Category { Id = 15, Name = "Outros", Type = TransactionType.Expense }
+            );
+        }
     }
 }
---
+++ Migrations/20260326202513_SeedCategories.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FinanceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Alimentação", 1 },
+                    { 2, "Transporte", 1 },
+                    { 3, "Salário", 0 },
+                    { 4, "Investimentos", 3 },
+                    { 5, "Lazer", 1 },
+                    { 6, "Moradia", 1 },
+                    { 7, "Saúde", 1 },
+                    { 8, "Educação", 1 },
+                    { 9, "Assinaturas", 1 },
+                    { 10, "Compras", 1 },
+                    { 11, "Freelance", 0 },
+                    { 13, "Dividendos", 3 },
+                    { 14, "Rendimentos", 3 },
+                    { 15, "Outros", 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 15);
+        }
+    }
+}
