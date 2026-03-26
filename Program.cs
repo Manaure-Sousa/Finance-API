d2a58038096482c98a663e97be07ed033267512d
Manaure Vasconcelos <manaurevasc97@gmail.com>
2026-03-26 01:39:24 -0300

    feat: integrate swagger for API documentation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=finance.db"));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "FinanceAPI";
+    config.Title = "FinanceAPI v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "FinanceAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
 
 app.MapTransactionsEndpoints();
 
